567c5a962b6fcc16f59545425fd543fdde42aa5a
antifly55 <antifly55@kookmin.ac.kr>
2023-03-25 03:08:34 +0900

    add: landing page scaffold

---
+++ ie3-commenter-frontend/src/Pages/LandingPage/index.js
@@ -0,0 +1,20 @@
+import HeaderComponent from "../../Components/HeaderComponent";
+import FooterComponent from "../../Components/FooterComponent";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <hr />
+
+      <div>
+        <h1>Hello Landing Page!</h1>
+      </div>
+
+      <hr />
+      <FooterComponent />
+    </div>
+  );
+};
+
+export default LandingPage;
--- ie3-commenter-frontend/src/index.js
+++ ie3-commenter-frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello React!</h1>
+    <App />
   </React.StrictMode>
 );
 
