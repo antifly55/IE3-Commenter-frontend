c590c5ee3173d870c5c751c5e749a10bef9faaf5
antifly55 <antifly55@kookmin.ac.kr>
2023-03-25 03:04:42 +0900

    add: footer component

---
+++ ie3-commenter-frontend/src/Components/FooterComponent/index.js
@@ -0,0 +1,14 @@
+const FooterComponent = () => {
+  return (
+    <div style={{ "text-align": "center", "margin-top": "2rem" }}>
+      <div style={{ "font-size": ".6rem" }}>
+        <span>공지사항</span> |<span>제휴문의</span> |<span>서비스 소개</span> |
+      </div>
+      <div style={{ "margin-top": "1rem" }}>
+        <h6>footer 제목</h6>
+      </div>
+    </div>
+  );
+};
+
+export default FooterComponent;
