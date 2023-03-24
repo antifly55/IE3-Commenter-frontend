c6bb4342e50ba5953b8e148c530429296aba269c
antifly55 <antifly55@kookmin.ac.kr>
2023-03-25 03:03:21 +0900

    add: header component

---
+++ ie3-commenter-frontend/src/Components/HeaderComponent/index.js
@@ -0,0 +1,17 @@
+const HeaderComponent = () => {
+  return (
+    <div style={{ "text-align": "center", margin: "2rem 0" }}>
+      <div>
+        <h1>header title</h1>
+      </div>
+      <div>
+        <span>nav1</span>
+        <span>nav2</span>
+        <span>nav3</span>
+        <span>nav4</span>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderComponent;
